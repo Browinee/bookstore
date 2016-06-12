c08b55127f91e68a65a6974549de7e82aeb8fbac
Justin <Nihilitypeo@gmail.com>
2016-06-13 00:26:05 +0800

    finished

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -9,4 +9,45 @@ myApp.controller('BooksController',['$scope','$http','$location','$routeParams',
 
   }
 
+  $scope.getBook = function(){
+      var id = $routeParams.id;
+      $http.get("/api/books/"+id).success(function(response){
+        $scope.book = response;
+      });
+
+
+  }
+
+
+
+  $scope.addBook = function(){
+
+      $http.post("/api/books/",$scope.book).success(function(response){
+      window.location.href="#/books";
+      });
+
+
+  }
+
+
+  $scope.updateBook = function(){
+  var id = $routeParams.id;
+      $http.put("/api/books/"+id,$scope.book).success(function(response){
+      window.location.href="#/books";
+      });
+
+
+  }
+
+
+  $scope.removeBook = function(id){
+
+      $http.delete("/api/books/"+id).success(function(response){
+      window.location.href="#/books";
+      });
+
+
+  }
+
+
 }]);
